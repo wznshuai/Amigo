532a6f106e78cdc448f1fd4d59340568f2ae2c84
WindFantasy <wangzenan01@chinatopcredit.com>
2016-12-02 11:50:07 +0800

    复现TabPageIndicator不居中的问题

--- app/src/main/java/me/ele/app/amigo/HomeActivity.java
+++ app/src/main/java/me/ele/app/amigo/HomeActivity.java
@@ -3,9 +3,14 @@ package me.ele.app.amigo;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.rey.material.widget.TabPageIndicator;
 
 import me.ele.amigo.Amigo;
 import me.ele.app.amigo.dev.DevActivity;
@@ -18,7 +23,11 @@ public class HomeActivity extends BaseActivity {
 
     private TextView infoView;
 
-    private TextView metaDataView;
+    TextView metaDataView;
+
+    TabPageIndicator indicator;
+
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,8 @@ public class HomeActivity extends BaseActivity {
 
         infoView = (TextView) findViewById(R.id.info);
         metaDataView = (TextView) findViewById(R.id.meta_data);
+        indicator = (TabPageIndicator) findViewById(R.id.indicator);
+        pager = (ViewPager) findViewById(R.id.pager);
         Log.e(TAG, "onCreate");
         Log.e(TAG, "getApplication from HomeActivity-->" + getApplication());
         Log.e(TAG, "version code from host-->" + Amigo.getHostPackageInfo(this, 0).versionCode);
@@ -40,6 +51,38 @@ public class HomeActivity extends BaseActivity {
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
+
+        pager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 3;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView((View)object);
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return false;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                TextView textView = new TextView(container.getContext());
+                textView.setText("asdada");
+                container.addView(textView);
+                return textView;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return "啊啊啊";
+            }
+        });
+
+        indicator.setViewPager(pager);
     }
 
     public void gotoDemo(View v) {
